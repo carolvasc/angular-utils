4ded06b2431b34182ad39dfd08ea28d0a82163f8
Carolina Vasconcelos de Souza <carolina.vsouza@totvs.com.br>
2021-02-09 21:36:48 -0300

    feat: diretiva para recuperar o tamanho da tela após redimensionamento
    * ótimo para media queries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CheckDecimalPlacesDirective } from './shared/directives/check-decimal-places.directive';
 import { ConcatenateArrayValuesPipe } from './shared/pipes/concatenate-array-values.pipe';
+import { WindowResizeDirective } from './shared/directives/window-resize.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ConcatenateArrayValuesPipe,
-    CheckDecimalPlacesDirective
+    CheckDecimalPlacesDirective,
+    WindowResizeDirective
   ],
   imports: [
     BrowserModule,
